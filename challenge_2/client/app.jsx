6912bda02acfd7acae21cf2db185fb369dcf9ad4
Melissa Ryan <melissaryandigital@gmail.com>
2021-05-26 14:08:32 -0500

    Initial commit - Adds basic dependencies, express server, react app

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Cryptocurrency Charting Tool</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
+
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+});
+
+app.listen(port, () => {
+  console.log(`Crypto chart tool listening at http://localhost:${port}`);
+})
 
