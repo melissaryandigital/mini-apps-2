74474fbfe29da88c031b4a04bd169c3eca625a4c
Melissa Ryan <melissaryandigital@gmail.com>
2021-05-26 14:49:35 -0500

    Renders sample chart.js

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -7,13 +7,20 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+
     }
   }
+
+  // https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-05-25&end=2021-05-25
+
 
   render() {
     return (
       <div>
         <h1>Cryptocurrency Charting Tool</h1>
+        <div>
+          <canvas id="myChart"></canvas>
+        </div>
       </div>
     )
   }
