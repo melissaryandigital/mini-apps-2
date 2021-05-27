631479f952a6cbba2119c69dad8026abeb1a0385
Melissa Ryan <melissaryandigital@gmail.com>
2021-05-27 10:28:31 -0500

    Completed BMR

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,35 +1,57 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import axios from 'axios';
 import "./styles.css";
-
-// const data = {
-//   labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-//   datasets: [
-//     {
-//       label: "USD",
-//       data: [33, 25, 35, 51, 54, 76],
-//       fill: false,
-//       borderColor: "#742774"
-//     }
-//   ]
-// };
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July"],
+        //isLoaded: false,
+        labels: [],
         datasets: [
           {
             label: "USD",
-            data: [33, 25, 35, 51, 54, 76, 55],
-            fill: false,
-            borderColor: "#742774"
+            data: [],
+            borderColor: "#742774",
           }
         ]
       }
     }
+  }
+
+  getHistoricalPrices() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-05-27&end=2021-05-27')
+      .then(response => {
+        let priceInfo = response.data.bpi;
+
+        let newLabels = [];
+        let newPrices = [];
+
+        for (const property in priceInfo) {
+          newLabels.push(property);
+          newPrices.push(priceInfo[property]);
+        }
+
+        let newData = { ...this.state.data };
+        newData.labels = newLabels;
+        newData.datasets[0].data = newPrices;
+        return newData;
+      })
+      .then(results => {
+        this.setState(
+          { data: results }
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+
+  componentDidMount() {
+    this.getHistoricalPrices();
   }
 
   render() {
